3ac17e0f7f0bc27baf4e72917621ce82a04e5d53
Adam Ferras <aferras@articulate.com>
2015-10-15 11:14:52 -0600

    fix

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
