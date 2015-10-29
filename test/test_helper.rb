1ce06f0381f4e39708c33f768044a67c488a385a
Adam Ferras <aferras@articulate.com>
2015-10-29 12:59:36 -0600

    Remove extra indentation for private methods

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -33,8 +33,8 @@ class ActiveSupport::TestCase
 
   private
 
-    # Returns true inside an integration test.
-    def integration_test?
-      defined?(post_via_redirect)
-    end
+  # Returns true inside an integration test.
+  def integration_test?
+    defined?(post_via_redirect)
+  end
 end
