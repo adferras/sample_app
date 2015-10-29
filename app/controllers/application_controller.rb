2c3c236cf6711363e455be1558c19d2b275bc7a1
Adam Ferras <aferras@articulate.com>
2015-10-29 12:13:08 -0600

    Refactor to use the ! to indicate a dangerous method in the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   private
 
     # Confirms a logged-in user.
-    def logged_in_user
+    def ensure_logged_in_user!
       unless logged_in?
         store_location
         flash[:danger] = "Please log in."
