7fa330226bb12f90a890c35604f8f891f029ab88
Adam Ferras <aferras@articulate.com>
2015-10-29 08:01:00 -0600

    Update production url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Mailer setup
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'hidden-shelf-4886.herokuapp.com'
+  host = 'sample-app-4886.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
